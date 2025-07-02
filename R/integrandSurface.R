0c93d8388ff2994f9e021a2a13f23badce8bdeae
Tingting Zhan <tingtingzhan@gmail.com>
2025-07-02 16:11:13 -0400

    tzh

--- R/integrandSurface.R
+++ R/integrandSurface.R
@@ -139,7 +139,7 @@ integrandSurface <- function(
     proj_beta = FALSE, # bug with my latest hyperframe !!!
     n = 501L,
     newid = min(3L, .row_names_info(newdata, type = 2L)) |> seq_len(), 
-    qlim = range(X, newX),
+    qlim = range(X[is.finite(X)], newX[is.finite(newX)]), # removing NA, NaN, Inf
     axis_col = c('dodgerblue', 'deeppink', 'darkolivegreen'),
     beta_col = 'purple',
     surface_col = 
