71580f4e364bcf059b067396f171fe3819ffb15e
Tingting Zhan <tingtingzhan@gmail.com>
2025-07-28 14:07:23 -0400

    tzh

--- R/onLoad_parallel.R
+++ R/onLoad_parallel.R
@@ -4,7 +4,7 @@
   
   Sys.setenv(
     '_R_CHECK_LIMIT_CORES_' = 'false',
-    # otherwise ?parallel:::.check_ncores causes error when ?devtools::check
+    # otherwise ?parallel:::.check_ncores causes error when ?devtools::check the quarto vignette (up to R 4.5.1)
     
     '_R_CHECK_SYSTEM_CLOCK_' = 0
     # https://stackoverflow.com/questions/63613301/r-cmd-check-note-unable-to-verify-current-time
@@ -13,9 +13,13 @@
   
   options(
     
-    mc.cores = switch(.Platform$OS.type, windows = 1L, detectCores()),
-    
-    use_unicode = TRUE
+    mc.cores = switch(
+      EXPR = .Platform$OS.type, # as of R 4.5, only two responses, 'windows' or 'unix'
+      windows = 1L, 
+      unix = detectCores()
+    )
+    # read this discussion very very carefully!  Especially Dirk's reply!!!!
+    # https://github.com/Rdatatable/data.table/issues/5658
     
   )
   
