6595d33a81b6e112bcb5fe6b63ed3ad28e714259
Tingting Zhan <tingtingzhan@gmail.com>
2026-03-16 08:51:42 -0400

    tzh

--- R/0PACKAGE.R
+++ R/0PACKAGE.R
@@ -13,3 +13,23 @@
 # [~~Highlighted strike through test.~~]{style="background-color: #FFFF00"}
 # [Highlighted test.]{style="background-color: #FFFF00"}
 
+
+
+#' @importFrom utils citation
+.onAttach <- function(libname, pkgname) {
+  
+  # .onAttach(libname = 'lib', pkgname = 'groupedHyperframe') # nah..
+  
+  'hyper.gam' |>
+    citation() |>
+    format(style = 'text') |> # utils:::format.citation
+    col_green() |>
+    style_bold() |>
+    cli_inform(class = 'packageStartupMessage')
+  
+  '\n' |>
+    cli_inform(class = 'packageStartupMessage')
+  
+}
+
+
